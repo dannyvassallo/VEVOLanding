3e0a96e78a459f592d674ceb91a49aa5828959fa
Dan Vassallo <danielvassallo87@gmail.com>
2014-06-23 11:24:58 -0400

    Refactored Code. Added Landing Area.

--- source/javascripts/_myscripts.js
+++ source/javascripts/_myscripts.js
@@ -27,3 +27,25 @@ $('.socials i, #share, #geton, input#geton').hover(function () {
 }, function () {
   $(this).removeClass("pulse");
 });
+
+
+//scroller
+
+
+
+$height = $(window).height();
+
+$("#geton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#sub-wrapper").offset().top
+    }, 2000);
+    $("#email").addClass("glowing-border");
+    $("#sub-wrapper").animate({ 
+        height: $height + 'px',
+      }, 1500 );
+    $("#margin").css("margin-top", "25%");
+    $("#sub-wrapper h3").css("font-size", "45px");
+    $("#sub-wrapper p.enter").css("font-size", "25px");
+    $("p.logo").hide();
+    $(".logo-lg > img").show();
+});
