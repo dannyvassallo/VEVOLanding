f64a6e2884d168ffd567ba7a3428a12086f4b9f1
Dan Vassallo <danielvassallo87@gmail.com>
2014-06-24 16:51:57 -0400

    Added Livereload Fixed Bottom and added FB info

--- source/javascripts/_myscripts.js
+++ source/javascripts/_myscripts.js
@@ -6,7 +6,13 @@ e.preventdefault;
 FB.ui(
   {
     method: 'share',
-    href: 'https://developers.facebook.com/docs/',
+    href: 'https://www.vydia.com/get-on-vevo/',
+    method: "feed",
+    link: 'https://www.vydia.com/get-on-vevo/',
+    name: "Get your music video on VEVO!",
+    caption: "Vydia.com makes getting your video on VEVO easy!",
+    description: "Make money from your music videos on Youtube, VEVO, Dailymotion, MUZU and more on one easy to use platform. No long term commitments and own 100% of your videos. As an official partner, you’ll be eligible for editorial placements on each platform to help drive awareness for your content.",
+    picture: "http://pullzone.vydiainc.netdna-cdn.com/wp-content/themes/vydia/images/vydiafacebookshare.jpg"
   },
   function(response) {
     if (response && !response.error_code) {
@@ -36,14 +42,12 @@ $height = $(window).height();
 $("#geton").click(function() {
     $('html, body').animate({
         scrollTop: $("#sub-wrapper").offset().top
-    }, 2000);
-    $("#email").addClass("glowing-border");
-    $("#sub-wrapper").animate({ 
-        height: $height + 'px',
-      }, 1500 );
-    $("#margin").css("margin-top", "25%");
+    }, 500);
     $("#sub-wrapper h3").css("font-size", "45px");
     $("#sub-wrapper p.enter").css("font-size", "25px");
     $("p.logo").hide();
     $(".logo-lg > img").show();
+    $("input#geton").addClass("pulse");
+    $("input#email").addClass("glow");
+    $("#margin").css("margin" , "100px 0 200px 0");
 });
